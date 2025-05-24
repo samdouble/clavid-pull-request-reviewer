bdbbd75461fb38da2e9694fc7f24f043b47a8c8f
samdouble <samleafs@protonmail.com>
2025-05-23 23:38:28 -0400

    feat: add simple call to Anthropic API

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
+const Anthropic = require('@anthropic-ai/sdk');
+
+const client = new Anthropic({
+  apiKey: core.getInput('anthropic_api_key') || process.env['ANTHROPIC_API_KEY'],
+});
 
 try {
   const additionalInstructions = core.getInput('additional_instructions');
   console.log(`Additional instructions: ${additionalInstructions}`);
+
+  const message = await client.messages.create({
+    max_tokens: 1024,
+    messages: [{ role: 'user', content: additionalInstructions }],
+    model: 'claude-3-7-sonnet-latest',
+  });
+  console.log(message.content);
+
   const time = (new Date()).toTimeString();
   core.setOutput("time", time);
   const payload = JSON.stringify(github.context.payload, undefined, 2)
